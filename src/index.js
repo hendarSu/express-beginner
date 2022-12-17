543c77c5272f7864b0fe03d19d2fbb4247b9b961
Hendar <hendarsundana@gmail.com>
2022-12-17 21:38:20 +0700

    +: Public assest

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const router = require("./routers/router");
 const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
+
+// Middleware Third party
+app.use(morgan("common"));
+
+// config folder public
+app.use(express.static("./src/public/assets"));
 
 app.use(express.json());
 app.use(express.urlencoded({
